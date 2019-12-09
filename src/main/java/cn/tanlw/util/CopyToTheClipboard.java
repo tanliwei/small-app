a81e28dbe13d4b326f5dbd6b701ef4c742d0ef95
tanliwei <tanliwei@users.noreply.github.com>
2019-12-09 10:37:42 +0800

    Create CopyToTheClipboard.java

---
+++ src/main/java/cn/tanlw/util/CopyToTheClipboard.java
@@ -0,0 +1,24 @@
+package cn.tanlw.util;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+
+/**
+ * https://stackoverflow.com/questions/6710350/copying-text-to-the-clipboard-using-java
+ */
+public class CopyToTheClipboard {
+    public static void main(String[] args) throws InterruptedException {
+        if (null == args || args.length == 0){
+            System.err.println("Need to input the content being copied to the system clipboard!");
+            return;
+        }
+        copyToTheClipboard(args[0]);
+        Thread.sleep(2000);
+    }
+
+    public static void copyToTheClipboard(String arg) {
+        StringSelection stringSelection = new StringSelection(arg);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+        System.out.println("The copy action has been done.");
+    }
+}
